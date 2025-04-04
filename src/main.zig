878f07d0b718367e48636d16c48ee1ee00d77f46
Brian <bphilip777@gmail.com>
2025-04-03 18:41:32 -0600

    Testing out the use of an enum within a packed struct

--- src/main.zig
+++ src/main.zig
@@ -2,11 +2,13 @@ const std = @import("std");
 const detachVsJoin = @import("DetachVsJoin.zig").main;
 const forLoopElse = @import("ForLoopElse.zig").main;
 const window = @import("Windows/Windows.zig").main;
+const bs = @import("BitSizes.zig").main;
 
 pub fn main() !void {
+    bs();
     // try detachVsJoin();
     // forLoopElse();
 
-    window();
+    // window();
     // std.debug.print("Result: {}\n", .{result});
 }
